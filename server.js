42bcb8bda5232a7280c971f8a992d6e3e2d6022c
John Aizov <3x6death@gmail.com>
2021-01-18 14:17:33 +0200

    table refactored

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const fetch = require('node-fetch');
 const express = require('express')
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    database: "jira",
+    password: "root"
+});
 
 const key = 'johnny.aizov@gmail.com:as246wBfHOsKPORfInckC52C'
 const url = 'https://aiiia.atlassian.net/'
@@ -8,20 +17,82 @@ const app = express()
 app.set("view engine", "hbs");
 
 
-app.get('/', async function (req, res) {
-    const issues = new Issues()
+app.get('/a/:filterId', async (req, res) => {
+
     const filters = new Filters()
     const filtersData = await filters.getAll()
-    const issuesData = await issues.getAll()
+    const issues = new Issues()
+    const filter = await filters.getById(req.params.filterId)
+
+    const issuesData = await issues.getIssuesByJql(filter.jql)
+    // console.log(issuesData)
+
+
     const [usersArr, statusesArr] = await generateTable(issuesData.issues)
-    console.log(usersArr)
+
     res.render("helloworld", {
         usersArr,
         statusesArr,
         filtersData
     });
+});
+
+app.get('/', async function (req, res) {
+    const issues = new Issues()
+    const filters = new Filters()
+    const filtersData = await filters.getAll()
+    const issuesData = await issues.getAll()
+    try {
+        const [usersArr, statusesArr] = await generateTable(issuesData.issues)
+        res.render("helloworld", {
+            usersArr,
+            statusesArr,
+            filtersData
+        });
+        LogToMySQL('ididi', 'GeneratedTable', '1')// true
+    } catch (e) {
+        LogToMySQL('ididi', 'GeneratedTable', '0')// false
+    }
+
+
 
 })
+
+function LogToMySQL(userId, action, isOk){
+    const date = require('moment')().format('YYYY-MM-DD')
+    const time = require('moment')().format('HH:mm:ss')
+    const sql =
+        "INSERT INTO logs(`userId`, `date`, `time`, `action`, `isOk`)" +
+        `VALUES('${userId}', '${date}', '${time}', '${action}', ${isOk})`;
+
+    pool.query(sql, function (err, results) {
+        if(err) console.log(err);
+        else console.log("Данные добавлены");
+    })
+
+}
+
+//
+// app.get('/a/:filterId', async function(req, res) {
+//     const filters = new Filters()
+//     const filtersData = await filters.getAll()
+//     const issues = new Issues()
+//     console.log('sddsdsds')
+//
+//     const filter = await filters.getById(req.params.filterId)
+//     // console.log(filter)
+//     const issuesData = await issues.getIssuesByJql(filter.jql)
+//     // console.log(issuesData)
+//     const [usersArr, statusesArr] = await generateTable(issuesData.issues)
+//
+//     res.render("helloworld", {
+//         filtersData,
+//         statusesArr,
+//         usersArr
+//     });
+//
+// });
+
 
 async function generateTable(issuesData) {
     const users = new Users()
@@ -30,49 +101,41 @@ async function generateTable(issuesData) {
     const statusData = await statuses.getAll()
 
     const statusesArr = []
+    const usersArr = []
     for(let a = 0; a < statusData.length; a++) {
-        statusesArr.push(statusData[a]['name'])
+        statusesArr.push([statusData[a]['name']])
     }
 
-    const usersArr = []
     for (let i = 0; i < usersData.length; i++) {
         if (usersData[i]['accountType'] === 'atlassian') {
-            const allStatuses = {}
-            for(let a = 0; a < statusData.length; a++) {
-                allStatuses[statusData[a]['id']] = 0
-            }
-            const statuesIds = {}
-            for(let a = 0; a < statusData.length; a++) {
-                statuesIds[statusData[a]['id']] = []
-            }
-
-            usersArr[i] = {
-                ['accountId']: usersData[i]['accountId'],
-                ['displayName']: usersData[i]['displayName'],
-                ['statuses']: allStatuses,
-                ['statusesIds']: statuesIds
-            }
-
-
-            console.log(i, usersArr[i])
-
+                usersArr[i] = {
+                    ['accountId']: usersData[i]['accountId'],
+                    ['displayName']: usersData[i]['displayName'],
+                    ['statuses']: [],
+                }
+                for(let a = 0; a < statusData.length; a++) {
+                    usersArr[i]['statuses'].push([statusData[a]['id'], {
+                        // name: statusData[a]['name'],
+                        amount: 0,
+                        ids: []
+                    } ])
+                    // console.log(usersArr[i])
+                }
+            // console.log(i, usersData[i])
             issuesData.forEach((issue) => {
 
                 // console.log(i, usersData[i])
                 if (issue['fields']['assignee']['accountId'] === usersData[i]['accountId']) {
+                    for(let a = 0; a < usersArr[i]['statuses'].length; a++) {
+                        if (issue['fields']['status']['id'] === usersArr[i]['statuses'][a][0]) {
+                            usersArr[i]['statuses'][a][1]['amount'] += 1
 
-                    for(let a = 0; a < statusData.length; a++) {
-                        if (issue['fields']['status']['id'] === statusData[a]['id']) {
-                            usersArr[i]['statuses'][issue['fields']['status']['id']] += 1
-
-                            usersArr[i]['statusesIds'][issue['fields']['status']['id']].push(issue['id'])
-                            // console.log(usersArr[i]['statusesIds'][issue['fields']['status']['id']].toString())
+                            usersArr[i]['statuses'][a][1]['ids'].push(issue['id'])
                         }
                     }
                 }
             })
 
-            // console.log(usersData)
         }
     }
     // console.log('TABLE: ', usersArr)
